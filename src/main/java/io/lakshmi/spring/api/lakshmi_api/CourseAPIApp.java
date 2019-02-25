e75af080b8eb5724b1dec950c4203634bf91b0ee
lakshmi-dasari <lakshmi.dasari1992@gmail.com>
2019-02-24 19:41:42 -0500

    spring code writing api

---
+++ src/main/java/io/lakshmi/spring/api/lakshmi_api/CourseAPIApp.java
@@ -0,0 +1,14 @@
+package io.lakshmi.spring.api.lakshmi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPIApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAPIApp.class, args);
+
+	}
+
+}
---
+++ src/main/java/io/lakshmi/spring/api/lakshmi_api/topics/TopicService.java
@@ -0,0 +1,40 @@
+package io.lakshmi.spring.api.lakshmi_api.topics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<> (Arrays.asList(
+			new Topic("spring", "springframework", "springframework description"),
+			new Topic("java","javasource","java description")));
+
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i=0;i<topics.size(); i++) {
+			Topic t= topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i,topic);
+				return;
+			}
+		}
+		
+	}
+	
+}
